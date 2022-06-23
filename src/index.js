e3d0d854a137f6b8f891e88cf07ed8ebba75f250
Dzmitry Panasevich <e002713@gmail.com>
2022-06-23 09:07:38 +0300

    feat: add function reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    const numberToString = n.toString();
+    let reverseStr = '';
+
+    for (let i = numberToString.length - 1; i >= 0; i--) {
+        reverseStr += numberToString[i];
+    }
+
+    return parseInt(reverseStr);
 }
